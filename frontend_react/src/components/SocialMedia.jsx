b2ec6438999355b81ca6db49039d3062b3b722a3
Tucker Brady <tuck.al.brady@gmail.com>
2022-04-11 21:01:04 -0600

    pushing latest build.

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,23 +4,26 @@ import { FaFacebook } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
-    <div className="app__social">
-      <a href="https://www.linkedin.com/in/tucker-a-brady/" target="_blank" rel="noreferrer">
-        <div>    
-              <BsLinkedin />
+      <div className="app__social">
+        <p className="p-text"><span>Connect</span> With Me</p>
+        <div className="app__social-icon-container">
+          <a href="https://www.linkedin.com/in/tucker-a-brady/" target="_blank" rel="noreferrer">
+            <div>    
+                  <BsLinkedin />
+            </div>
+          </a>
+          <a href="https://www.instagram.com/tucker_al/" target="_blank" rel="noreferrer">
+            <div>
+                  <BsInstagram />
+            </div>
+          </a>
+          <a href="https://www.facebook.com/tucker.a.brady" target="_blank" rel="noreferrer">
+            <div>
+                  <FaFacebook />
+            </div>
+          </a>
         </div>
-      </a>
-      <a href="https://www.instagram.com/tucker_al/" target="_blank" rel="noreferrer">
-        <div>
-              <BsInstagram />
-        </div>
-      </a>
-      <a href="https://www.facebook.com/tucker.a.brady" target="_blank" rel="noreferrer">
-        <div>
-              <FaFacebook />
-        </div>
-      </a>
-    </div>
+      </div>
   )
 }
 
